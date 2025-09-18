7d4446c179da9187c5560f25edc634a203906419
Nkiko Hertier <afrigames123@gmail.com>
2025-09-18 09:04:55 +0000

    Adding tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./CTFd/themes/core/templates/**/*.html",
+    "./CTFd/themes/admin/templates/**/*.html",
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
